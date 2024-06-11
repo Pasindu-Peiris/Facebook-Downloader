d850837157e99a286d99e0b72ccc8ae050a2700f
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-11 15:17:02 +0530

    Create Downbody.jsx

---
+++ frontend/src/pages/Downbody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Downbody = () => {
+  return (
+    <div className=' w-[100%] h-[80vh] flex items-center justify-center'>
+
+
+
+
+    </div>
+  )
+}
+
+export default Downbody
