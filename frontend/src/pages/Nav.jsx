ef88fa6615044bbe8428991d64d320c757658bc1
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-11 14:55:53 +0530

    Update

--- frontend/src/pages/Nav.jsx
+++ frontend/src/pages/Nav.jsx
@@ -5,11 +5,16 @@ const Nav = () => {
   return (
     <div>
 
-        <nav className=' bg-stone-700 w-[100%] h-20 flex items-center justify-around '>
+        <nav className='w-[100%] h-20  bg-sky-950 flex items-center justify-around border-b-2'>
 
             <div className="logo text-white">
-                <a href='!' className=' text-3xl  flex items-center justify-center'><span className='p-2'><img src={Imf} alt=""  width={40}/></span>Face Save</a>
+                <a href='!' className=' text-xl flex items-center justify-start'><p><span className=' text-blue-600'>FB</span> Save</p></a>
             </div>
+
+            <ul className='menu flex items-center gap-6 text-white'>
+            
+              <li><a href='!' className=' capitalize'>How to?</a></li>
+            </ul>
 
 
         </nav>
