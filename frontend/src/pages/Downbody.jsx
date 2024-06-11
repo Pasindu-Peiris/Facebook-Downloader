8323f106b80e6716d41930da693f1f72de013290
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-11 16:02:23 +0530

    Update Downbody.jsx

--- frontend/src/pages/Downbody.jsx
+++ frontend/src/pages/Downbody.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Downbody = () => {
+
+    const [link , setLink] = useState('')
+
+
+    const hadleSubmit = (e) => {
+
+        e.preventDefault();
+
+        alert(link)
+
+
+
+    }
+
+
+
     return (
         <div className=' w-[100%] h-[80vh] flex items-center justify-center'>
 
             <div className=' logo block'>
                 <div className="logo  text-center">
                     <a href='!' className=' text-3xl'><p><span className=' text-blue-600'>FB </span>.Save</p></a>
-                    <p className=' text-xl p-3'>Facebook Video Online Free Downloader</p>
+                    <p className=' text-xl p-2'>Facebook Video Online Free Downloader</p>
+                    <p className=' text-gray-500'>Download Facebook Videos Online</p>
                 </div>
 
-                <div className="inputbox">
+                <div className="inputbox mt-6">
 
-                    <form >
+                    <form onSubmit={hadleSubmit} >
 
-                        <input type="link" name="link" id="link" className=' w-[600px] h-[9vh] p-3 outline-none mx-2' />
+                        <input type="link"
+
+                        onChange={ (e) => {
+                            setLink(e.target.value);
+                        }}
+
+                        name="link" id="link" className=' w-[550px] h-[9vh] p-3 outline-none mx-2' placeholder='Enter Facebook Video Download Link....' />
                         <input type="submit" value="Download" className=' text-white w-[120px] h-[9vh] bg-sky-950 p-3 outline-none' />
 
                     </form>
