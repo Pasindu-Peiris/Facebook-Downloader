34435d1fbfd38312112c0ad7639ba44c46ee16d2
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-11 15:03:02 +0530

    Complete Nav Bar

--- frontend/src/pages/Nav.jsx
+++ frontend/src/pages/Nav.jsx
@@ -5,14 +5,14 @@ const Nav = () => {
   return (
     <div>
 
-      <nav className='w-[100%] h-20  bg-sky-950 flex items-center justify-around border-b-2'>
+      <nav className='w-[100%] h-20  bg-sky-950 flex items-center justify-around border-b-2 fixed'>
 
         <div className="logo text-white">
           <a href='!' className=' text-2xl flex items-center justify-start'><p><span className=' text-blue-600'>FB </span>.Save</p></a>
         </div>
 
         <ul className='menu flex items-center gap-6 text-white'>
-          <li><a href='!' className=' capitalize text-xl hover:text-blue-600'>How to?</a></li>
+          <li><a href='!' className=' capitalize text-xl hover:text-blue-600'>How to ?</a></li>
         </ul>
 
       </nav>
