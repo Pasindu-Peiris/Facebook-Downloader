5b69b4352e4f047db543a1e38912c31d3896a9fc
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-12 18:13:49 +0530

    Update Downbody.jsx

--- frontend/src/pages/Downbody.jsx
+++ frontend/src/pages/Downbody.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Downbody = () => {
 
-    const [link , setLink] = useState('')
+    const [slink, setLink] = useState('')
+    const [vdata, setvdata] = useState(null)
 
 
     const hadleSubmit = (e) => {
 
         e.preventDefault();
 
-        alert(link)
+        axios.post('http://localhost:8090/links/getdata', { slink }).then((res) => {
+            console.log(res.data.data);
+            setvdata(res.data.data)
+
+        })
 
 
 
@@ -18,7 +24,7 @@ const Downbody = () => {
 
 
     return (
-        <div className=' w-[100%] h-[80vh] flex items-center justify-center'>
+        <div className=' w-[100%] h-[100vh] flex items-center justify-center'>
 
             <div className=' logo block'>
                 <div className="logo  text-center">
@@ -27,22 +33,44 @@ const Downbody = () => {
                     <p className=' text-gray-500'>Download Facebook Videos Online</p>
                 </div>
 
-                <div className="inputbox mt-6">
+                {
+                    vdata == null ?
 
-                    <form onSubmit={hadleSubmit} >
+                        <div className="inputbox mt-6">
 
-                        <input type="url"
+                            <form onSubmit={hadleSubmit} >
 
-                        onChange={ (e) => {
-                            setLink(e.target.value);
-                        }}
+                                <input type="url"
 
-                        name="link" id="link" className=' w-[550px] h-[9vh] p-3 outline-none mx-2' placeholder='Enter Facebook Video Download Link....' />
-                        <input type="submit" value="Download" className=' text-white w-[120px] h-[9vh] bg-sky-950 p-3 outline-none' />
+                                    onChange={(e) => {
+                                        setLink(e.target.value);
+                                    }}
 
-                    </form>
+                                    name="link" id="link" className=' w-[550px] h-[9vh] p-3 outline-none mx-2' placeholder='Enter Facebook Video Download Link....' />
+                                <input type="submit" value="Download" className=' text-white w-[120px] h-[9vh] bg-sky-950 p-3 outline-none' />
 
-                </div>
+                            </form>
+
+                        </div>
+                        :
+                        <div className=' grid items-center justify-center'>
+
+                            <div className="flex mt-6 items-center justify-center mp4v w-[250px] h-[200px] bg-sky-950 overflow-hidden">
+                                {/* <video width="320" height="240">
+                                    <source src={vdata[0].url} type="video/mp4" />
+
+                                </video> */}
+                                <img src={vdata[0].thumbnail} alt=""  />
+                            </div>
+
+
+
+                            <a href={vdata[0].url} download="proposed_file_name"> Download</a>
+
+
+
+                        </div>
+                }
 
             </div>
 
