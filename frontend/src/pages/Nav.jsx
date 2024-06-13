1f9ce0c26e7c58e8bb6e399e53f916dbbf0cc516
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-13 14:00:34 +0530

    Update Nav.jsx

--- frontend/src/pages/Nav.jsx
+++ frontend/src/pages/Nav.jsx
@@ -11,9 +11,9 @@ const Nav = () => {
           <a href='!' className=' text-2xl flex items-center justify-start'><p><span className=' text-[#2D68C4] '>FB </span>.Save</p></a>
         </div>
 
-        <ul className='menu flex items-center gap-6 text-white'>
+        {/* <ul className='menu flex items-center gap-6 text-white'>
           <li><a href='!' className=' capitalize text-sm hover:text-[#003153] rounded'>How to ?</a></li>
-        </ul>
+        </ul> */}
 
       </nav>
 
