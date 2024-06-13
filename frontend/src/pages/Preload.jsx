ff395425035746c1c13df7155125a71d908477d7
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-13 17:10:07 +0530

    Create Preload.jsx

---
+++ frontend/src/pages/Preload.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Blocks } from 'react-loader-spinner'
+
+
+const Preload = () => {
+    return (
+        <div className=' flex items-center justify-center w-[100%] h-[45vh]'>
+
+
+            <Blocks
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                visible={true}
+            />
+
+
+        </div>
+    )
+}
+
+export default Preload
