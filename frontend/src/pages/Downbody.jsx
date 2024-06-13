8cf77d1c7d0d11ed32b093e7337c95351466ce79
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-13 17:10:20 +0530

    Update Downbody.jsx

--- frontend/src/pages/Downbody.jsx
+++ frontend/src/pages/Downbody.jsx
@@ -1,7 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Preload from './Preload'
+import Joyride, { STATUS } from 'react-joyride';
 
 const Downbody = () => {
+
+
+
+    const [{ run, steps }, setState] = useState({
+        run: true,
+        steps:
+            [
+                {
+
+                    content: <h2 className='text-xl text-center'> Welcom to FB.Save  <br/>Let's begin Tour 🛩️ </h2>,
+                    locale: { skip: 'Skip tutorial' },
+                    placement: 'center',
+                    target: "#bbody"
+
+                },
+                {
+
+                    content: <h2 className='text-xl'>Enter Your Video Link 🚀</h2>
+                    ,
+                    locale: { skip: 'Skip tutorial' },
+                    placement: 'bottom',
+                    target: "#bot",
+                    title: "First Step"
+
+                },
+                {
+
+                    content: <h2 className='text-xl'>Now Click Download Button 🔍</h2>,
+                    locale: { skip: 'Skip tutorial' },
+                    placement: 'bottom',
+                    target: "#step1",
+                    title: "Second Step"
+
+                },
+                
+            ]
+
+    })
 
     const [slink, setLink] = useState('')
     const [vdata, setvdata] = useState(null)
@@ -11,6 +51,10 @@ const Downbody = () => {
 
         e.preventDefault();
 
+        axios.post('http://localhost:8090/links/add', { slink }).then((res) => {
+           console.log("add")
+        })
+
         axios.post('http://localhost:8090/links/getdata', { slink }).then((res) => {
             console.log(res.data);
             setvdata(res.data.data)
@@ -19,13 +63,34 @@ const Downbody = () => {
 
     }
 
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+
+        setTimeout(() => {
+
+            setIsLoading(false); // Set isLoading to false when data is ready
+        }, 11000); // Simulate a 2-second data loading delay
+
+    })
+
 
     return (
         <div className=' w-[100%] h-[100vh] flex items-center justify-center'>
 
+            <Joyride
+                continuous
+                callback={() => { }}
+                run={run}
+                steps={steps}
+                hideCloseButton
+                scrollToFirstStep
+                showSkipButton
+                showProgress />
+
             <div className=' logo block'>
                 <div className="logo  text-center">
-                    <a href='!' className=' text-4xl'><p><span className=' ' style={{ color: "#4D4DFF" }}>FB </span>.Save </p> </a>
+                    <a href='!' className=' text-4xl' ><p id={`bbody`}><span className=' ' style={{ color: "#4D4DFF" }}>FB </span>.Save </p> </a>
                     <p className=' xl:text-xl sm:text-sm p-2'>Facebook Video Online Free Downloader</p>
                     <p className=' text-gray-500'>Download Facebook Videos Online</p>
                 </div>
@@ -43,29 +108,34 @@ const Downbody = () => {
                                         setLink(e.target.value);
                                     }}
 
-                                    name="link" id="link" className=' md:w-[450px] xl:w-[550px] h-[9vh] p-3 outline-none mx-2 border-solid border-2 rounded-md border-[#003153]' placeholder='Enter Facebook Video Download Link....' />
-                                <input type="submit" value="Download" className=' text-white w-[120px] h-[9vh] rounded-md bg-[#003153]  outline-none' />
+                                    name="link" id={`bot`} className=' md:w-[450px] xl:w-[550px] h-[9vh] p-3 outline-none mx-2 border-solid border-2 rounded-md border-[#003153]' placeholder='Enter Facebook Video Download Link....' />
+                                <input type="submit" id={`step1`} value="Download" className=' cursor-pointer text-white w-[120px] h-[9vh] rounded-md bg-[#003153]  outline-none' />
 
                             </form>
 
-                        </div>
-                        :
-                        <div className='xl:flex sm:grid xl:justify-between items-center mt-8 p-6 sm:w-[100%] md:w-[100%] xl:w-[500px] sm:h-[100%] md:h-[100%] xl:h-[300px] bg-gray-200'>
-
-                            <div className="flex items-center justify-center  w-[260px] h-[250px] bg-sky-950 overflow-hidden">
-                                <img src={vdata[0].thumbnail} alt="" />
-                            </div>
-
-                            <div className=' grid items-center justify-center xl:gap-8 sm:gap-8 md:gap-8'>
-
-                                <a href={vdata[0].url} download="proposed_file_name" className=' bg-slate-800 text-white p-3 rounded-sm mt-5'> Download 720</a>
-                                <a href={vdata[0].url} download="proposed_file_name" className=' bg-slate-800 text-white p-3 rounded-sm mt-5'> Download 1080</a>
-
-                            </div>
 
 
 
                         </div>
+                        : <>
+
+                            {isLoading ? <Preload /> : <div className='xl:flex sm:grid xl:justify-between items-center mt-8 p-6 sm:w-[100%] md:w-[100%] xl:w-[500px] sm:h-[100%] md:h-[100%] xl:h-[300px] bg-gray-200'>
+
+                                <div className="flex items-center justify-center  w-[260px] h-[250px] bg-sky-950 overflow-hidden">
+                                    <img src={vdata[0].thumbnail} alt="" />
+                                </div>
+
+                                <div className=' grid items-center justify-center xl:gap-8 sm:gap-8 md:gap-8'>
+
+                                    <a href={vdata[0].url} download="proposed_file_name" className=' bg-slate-800 text-white p-3 rounded-sm mt-5' id={`map1`}> Download {vdata[0].resolution}</a>
+                                    <a href={vdata[1].url} download="proposed_file_name" className=' bg-slate-800 text-white p-3 rounded-sm mt-5'> Download {vdata[1].resolution}</a>
+
+                                </div>
+
+                            </div>}
+
+
+                        </>
                 }
 
             </div>
